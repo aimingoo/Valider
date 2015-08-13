45493ea542759cf8b80e0a7fc2434f19e166f76f
aimingoo <aimingoo@wandoujia.com>
2015-08-13 09:29:27 +0800

    release v1.1

---
+++ README.md
@@ -0,0 +1,78 @@
+# Valider
+The Valider module to check rate of invalid action/event/behaviour, and return a final status to you.
+
+Valider include features:
+	- continuous invalid X times, or
+	- invalid X times in Y seconds
+will report as once. and
+	- support multi-tags in a checker
+	- very clean & small, little memory, and faster!
+
+# Install & Usage
+download the Valider.lua file and put into lua search path or current directory.
+
+load it as a file module from lua. use Valider:new() to get checker object.
+```lua
+local Valider = require('Valider')
+local checker = Valider:new()
+
+if checker:invalid('tag') then
+	-- do something when status is invalid
+	..
+end
+```
+
+# Options
+
+you can create checker with options.
+```lua
+local options = {
+	-- count one continuous-invalid, when less maxContinuousInterval between two invalids
+	maxContinuousInterval = 3,
+
+	-- report as once when continuous-invalid count >= maxContinuous
+	--	*) continuous invalid X times
+	maxContinuous = 100,
+
+	-- report as once when invalid count >= maxTimes, in maxSeconds
+	--	*) invalid X times in Y seconds history
+	maxTimes = 30,
+	maxSeconds = 30,
+}
+
+-- get checker with options
+--	*) or <nil> for default
+local checker = Valider:new(options)
+
+..
+```
+
+# Multi-tags checker
+with tags, you can check multi source action/event/behaviour. the tag is anything(string/boolean/table in lua). for examples:
+```lua
+local checker = Valider:new()
+
+if checker:invalid('network hardware invalid') then
+	..
+end
+
+if checker:invalid('server connection lost') then
+	..
+end
+
+if (checker:invalid(a_table_variant) or
+	checker:invalid(a_boolean_variant) or
+	checker:invalid(any_data_as_key_of_lua_table)) then
+	..
+end
+```
+
+# testcase or samples
+some examples in testcase/t_base_demo.lua. require time.lua module and run it with luajit. the module  from:
+	- https://luapower.com/time/
+and, install luajit in macos(only for the testcase):
+	> brew install luajit
+run:
+	> git clone https://github.com/aimingoo/Valider
+	> cd Valider/testcase
+	> luajit t_base_demo.lua
---
+++ testcase/t_base_demo.lua
@@ -0,0 +1,134 @@
+--------------------------------------------------------
+--
+-- the test of Valider.lua module in NGX_4C
+--
+-- Note:
+--	need luajit for lib/time.lua, the testcase only.
+--------------------------------------------------------
+
+local sleep = require('lib.time').sleep
+local Valider = dofile('../Valider2.lua')
+
+--------------------------------------------------------
+--
+-- base and static samples
+--
+--------------------------------------------------------
+local status = {
+	false, false, false, false, false, false, false, true, false, false, true, false, false, false, false, false, false, false, 	-- 16
+	false, false, true, false, false, false, false, false, false, false, false, false, false, false, false,							-- 14
+	false, false, false, false, true, false, true, false, false, false, true, false, true, false, false, false, false, false,		-- 14
+	false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, 	-- 18
+	false, false, false, false, true, false, true, false, false, false, true, false, true, false, false, false, false, false,		-- 14
+	false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, 	-- 17
+	false, false, false, false, true, false, true, false, false, false, true, false, true, false, false, false, false, false,		-- 14
+}
+
+--
+-- test case 1
+--		break at #33	invalid over maxTimes:	30
+--
+
+-- checker with default setting
+local checker = Valider:new({
+	-- maxContinuousInterval = 3,
+	-- maxContinuous = 100,	-- continuous invalid X times (default 100), or
+	-- maxTimes = 30,			-- invalid X times in Y seconds history (default is 30 times in 30s)
+	-- maxSeconds = 30			-- (max history seconds, default is 30s)
+})
+print('==> testcase 1')
+local now = os.time()
+for i, state in ipairs(status) do
+	if not state and checker:invalid('case1') then
+		print('run ' .. tostring(os.time()-now) .. 's',
+			'break at #' .. tostring(i),
+			'invalid over maxTimes:', checker.maxTimes)
+		break
+	end
+	print(i, '-->', state)
+	sleep(0.1)
+end
+
+--
+-- test case 2, output
+--		break at #115	invalid over maxContinuous:	100
+--
+
+-- invald 100 continuous and interval < 3s
+local checker = Valider:new({
+	maxContinuousInterval = 3,
+	maxContinuous = 100,
+	maxTimes = 30000,	-- or, 30000 times in 30s
+})
+print('\n==> testcase 2')
+local now = os.time()
+for i, state in ipairs(status) do
+	if not state and checker:invalid('case2') then
+		print('run ' .. tostring(os.time()-now) .. 's',
+			'break at #' .. tostring(i),
+			'invalid over maxContinuous:', checker.maxContinuous)
+		break
+	end
+	if i%10 == 0 then print(i, '-->', state) end
+end
+
+--------------------------------------------------------
+--
+-- random samples testcases
+--
+--------------------------------------------------------
+
+--
+-- test case 3, random
+--
+
+-- invald 3 continuous and interval < 3s
+local checker = Valider:new({
+	maxContinuousInterval = 8,
+	maxContinuous = 5,
+	maxTimes = 30000,	-- or, 30000 times in 30s
+})
+print('\n==> testcase 3')
+local now = os.time()
+math.randomseed(os.time())
+while true do
+	local state = (math.random(100) % 2) == 0
+	if not state and checker:invalid('case3') then
+		print('run ' .. tostring(os.time()-now) .. 's',
+			'break, invalid over maxContinuous in maxContinuousInterval(8s):', checker.maxContinuous)
+		break
+	end
+
+	print('-->', state,	checker['case3'] and checker['case3'][4])
+	sleep(1)
+end
+
+
+--
+-- test case 4, multi tags
+--
+
+-- invald 3 continuous and interval < 3s
+--	1) skip re-create, use more tags in the <checker>
+print('\n==> testcase 4')
+local now = os.time()
+math.randomseed(os.time())
+while true do
+	local state1 = (math.random(100) % 2) == 0
+	if not state1 and checker:invalid('tag1') then
+		print('break in tag1, invalid over maxContinuous in maxContinuousInterval(8s):', checker.maxContinuous)
+		break
+	end
+
+	local state2 = (math.random(5) % 2) == 0
+	if not state2 and checker:invalid('tag2') then
+		print('break in tag2, invalid over maxContinuous in maxContinuousInterval(8s):', checker.maxContinuous)
+		break
+	end
+
+	print('--> tag1/tag2 is: ',
+		table.concat({tostring(state1), tostring(state2)}, '/'),
+		table.concat({checker.tag1 and tostring(checker.tag1[4]),
+					  checker.tag2 and tostring(checker.tag2[4])}, '/'))
+	sleep(1)
+end
